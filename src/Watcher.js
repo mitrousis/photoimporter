952373346fa71c1df8514dd394b2149d65137cc1
Nicholas Mitrousis <nicholas.mitrousis@gmail.com>
2019-11-18 22:27:29 -0800

    wip: SD card reading

--- src/Watcher.js
+++ src/Watcher.js
@@ -2,12 +2,16 @@ const chokidar = require('chokidar')
 const debounce = require('throttle-debounce').debounce
 const EventEmitter = require('events')
 const path = require('path')
+const drivelist = require('drivelist')
 
 class Watcher extends EventEmitter {
   constructor () {
     super()
     this._changeTriggerDelay = 5000
     this._changePollingInterval = 10000
+
+    this._sdCardPollingInterval = 4000
+    this._sdCardPollingTimeoutRef = null
   }
 
   watch (watchDirPath) {
@@ -33,19 +37,52 @@ class Watcher extends EventEmitter {
   }
 
   // This is just a test currently
-  watchForMountedVolume (volumeName) {
-    // One-liner for current directory
-    const watchVolumePath = path.join('/Volumes/', volumeName)
+  // watchForMountedVolume (volumeName) {
+  //   // One-liner for current directory
+  //   const watchVolumePath = path.join('/Volumes/')
 
-    this._chokidarWatcher = chokidar.watch(watchVolumePath, {
-      usePolling: true,
-      interval: 1000
-    })
+  //   this._chokidarWatcher = chokidar.watch(watchVolumePath, {
+  //     usePolling: true,
+  //     interval: 1000,
+  //     depth: 0
+  //   })
 
-    this._chokidarWatcher.on('add', (event, path) => {
-      console.log(event, path)
+  //   this._chokidarWatcher.on('addDir', (event, path) => {
+  //     console.log(event, path)
+  //   })
+  // }
+
+  pollForSDCard () {
+    this._sdCardPollingTimeoutRef = setTimeout(() => {
+
     })
+  }
+
+  listSDCards () {
+    return drivelist.list()
+      .then((drives) => {
+        return drives.reduce((sdDrives, drive) => {
+          if (drive.isCard) {
+            return sdDrives.concat({
+
+            })
+          }
+        }, [])
+
+        forEach((drive) => {
+          if (drive.isCard) {
+            console.log(JSON.stringify(drive.mountpoints, null, 1))
+          }
+        })
+      })
   }
 }
 
 module.exports = Watcher
+
+drivelist.list()
+  .then((drives) => {
+    drives.forEach((drive) => {
+      console.log(JSON.stringify(drive.mountpoints, null, 1))
+    })
+  })
